5e9a5c03292fcfc4e93a3e16fb0497f24e2f9c3c
Seospan <sophie.jobez@gmail.com>
2024-04-29 17:36:55 +0200

    Added things

--- grist-widgets/recipe_toprint/recipe.js
+++ grist-widgets/recipe_toprint/recipe.js
@@ -1,16 +1,16 @@
-grist.ready();
-      grist.onRecord(function(record) {
+grist.ready({columns: ['Titre', 'Texte']});
+grist.onRecord(function(record) {
+try {
+    data.status = '';
+    if (row === null) {
+        throw new Error("(No data - not on row - please add or select a row)");
+    }
+    console.log("GOT...", JSON.stringify(row));
+    if (row.References) {
         try {
-            data.status = '';
-            if (row === null) {
-              throw new Error("(No data - not on row - please add or select a row)");
-            }
-            console.log("GOT...", JSON.stringify(row));
-            if (row.References) {
-              try {
-                Object.assign(row, row.References);
-              } catch (err) {
-                throw new Error('Could not understand References column. ' + err);
-              }
-            }
-      });
+        Object.assign(row, row.References);
+        } catch (err) {
+        throw new Error('Could not understand References column. ' + err);
+        }
+    }
+});
