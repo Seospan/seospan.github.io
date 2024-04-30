4b9d41c83925720e82ea5ee3b550fa6515c3ee46
Seospan <sophie.jobez@gmail.com>
2024-04-30 12:11:36 +0200

    cleaned Vue for a fresh start

--- grist-widgets/recipe_toprint/recipe.js
+++ grist-widgets/recipe_toprint/recipe.js
@@ -8,30 +8,16 @@ function ready(fn) {
     }
 }
 
-const data = {
-    count: 0,
-    invoice: '',
-    status: 'waiting',
-    tableConnected: false,
-    rowConnected: false,
-    haveRows: false,
-  };
-  let app = undefined;
 
 function handleError(err) {
     console.error(err);
-    const target = app || data;
-    target.invoice = '';
-    target.status = String(err).replace(/^Error: /, '');
-    console.log("ERROR : ");
-    console.log(data);
+    console.log("ERROR");
 }
 
 function updateRecipe(row, mappings){
     console.log("TEST");
 
     try {
-        data.status = '';
         if (row === null) {
             throw new Error("(No data - not on row - please add or select a row)");
         }
@@ -67,29 +53,4 @@ ready(function(){
     console.log("GOGOGO");
     grist.ready({columns: ['Titre', 'Texte'], requiredAccess: 'read table'});
     grist.onRecord(updateRecipe);
-
-    // Monitor status so we can give user advice.
-    grist.on('message', msg => {
-        // If we are told about a table but not which row to access, check the
-        // number of rows.  Currently if the table is empty, and "select by" is
-        // not set, onRecord() will never be called.
-        if (msg.tableId && !app.rowConnected) {
-        grist.docApi.fetchSelectedTable().then(table => {
-            if (table.id && table.id.length >= 1) {
-            app.haveRows = true;
-            }
-        }).catch(e => console.log(e));
-        }
-        if (msg.tableId) { app.tableConnected = true; }
-        if (msg.tableId && !msg.dataChange) { app.RowConnected = true; }
-    });
-
-    Vue.config.errorHandler = function (err, vm, info)  {
-        handleError(err);
-      };
-    
-    app = new Vue({
-        el: '#app',
-        data: data
-      });
 })
