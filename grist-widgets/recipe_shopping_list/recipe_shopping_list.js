3da252ad2ad1bcd2c9622f0c97ff31c4c64f0eaa
Seospan <sophie.jobez@gmail.com>
2024-05-01 21:53:44 +0200

    u

--- grist-widgets/recipe_shopping_list/recipe_shopping_list.js
+++ grist-widgets/recipe_shopping_list/recipe_shopping_list.js
@@ -156,8 +156,8 @@ ready(function(){
                 for(rayon in shopping_list){
                     console.log("LOG RAYON");
                     console.log(rayon);
-                    console.log(typeof(shopping_list[rayon]['items']));
-                    console.log(shopping_list[rayon]['items']);
+                    console.log(typeof(shopping_list[rayon]['items']['value']));
+                    console.log(shopping_list[rayon]['items']['value']);
                     
                     //var shopping_list_items = JSON.parse(shopping_list[rayon]['items']);
                     //console.log(shopping_list_items)
