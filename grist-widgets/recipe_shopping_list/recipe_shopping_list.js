dc26f1ea79ed2744a1f663db0f5ebd16a0f202d0
Seospan <sophie.jobez@gmail.com>
2024-05-01 22:31:10 +0200

    Update recipe_shopping_list.js

--- grist-widgets/recipe_shopping_list/recipe_shopping_list.js
+++ grist-widgets/recipe_shopping_list/recipe_shopping_list.js
@@ -176,7 +176,8 @@ ready(function(){
                             console.log("ITEM");
                             console.log(shopping_list_items[item]);
                             html_shopping_list += "<tr>";
-                            html_shopping_list += shopping_list_items[item]['qte_totale']+" "+shopping_list_items[item]['unite'];
+                            html_shopping_list += "<td>"+shopping_list_items[item]['nom']+"</td>";
+                            html_shopping_list += "<td>"+shopping_list_items[item]['qte_totale']+" "+shopping_list_items[item]['unite']+"</td>";
                             html_shopping_list += "</tr>"
                         }
                         html_shopping_list += '</tbody>';
