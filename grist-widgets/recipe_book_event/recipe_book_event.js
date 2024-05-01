06d0d6b56b80a181639d36da98c6019cca070cc4
Seospan <sophie.jobez@gmail.com>
2024-05-01 19:37:51 +0200

    u

--- grist-widgets/recipe_book_event/recipe_book_event.js
+++ grist-widgets/recipe_book_event/recipe_book_event.js
@@ -8,6 +8,17 @@ function ready(fn) {
     }
 }
 
+function printDiv(divName){
+    var printContents = document.getElementById(divName).innerHTML;
+    var originalContents = document.body.innerHTML;
+
+    document.body.innerHTML = printContents;
+
+    window.print();
+
+    document.body.innerHTML = originalContents;
+
+}
 
 function handleError(err) {
     console.error(err);
