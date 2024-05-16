f739df4d258e022c6b62f6334dddf8ed026fbc59
Seospan <sophie.jobez@gmail.com>
2024-05-16 16:49:59 +0200

    Update recipe_shopping_list.js

--- grist-widgets/recipe_shopping_list/recipe_shopping_list.js
+++ grist-widgets/recipe_shopping_list/recipe_shopping_list.js
@@ -42,7 +42,7 @@ function calculate_shopping_list(details_ingredients_evenement){
         console.log("Entering repas");
         for(recette in repas){
             console.log("Entering recette");
-            console.log(repas);
+            console.log(recette);
             for(ingredient_from_recette in recette.ingredients){
                 console.log("Entering ingredient");
                 nom_ing = ingredient_from_recette.nom;
