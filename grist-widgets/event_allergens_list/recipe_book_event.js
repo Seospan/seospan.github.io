2190088d46e12adff9fefd60eafb175434ee2628
Seospan <sophie.jobez@gmail.com>
2026-07-09 17:29:35 +0200

    Cleaned duplicated widget

--- grist-widgets/event_allergens_list/recipe_book_event.js
+++ grist-widgets/event_allergens_list/recipe_book_event.js
@@ -15,81 +15,6 @@ $( document ).ready(function() {
         console.log("ERROR");
     }
     
-    function recipeToHTML(nom_evenement, recette_contexte, repas_lie){
-    
-        note_contexte_recette = recette_contexte.note_contexte_recette;
-        nom_session_cuisine = recette_contexte.nom_session_cuisine;
-        recette = recette_contexte.details_recette;
-        nb_portions = recette_contexte.nb_portions;
-        multip_recette = recette_contexte.nb_portions / recette_contexte.details_recette.Nombre_de_personnes;
-        recipe_ingredients = recette_contexte.ingredients;
-        //console.log("MAPPED");
-        //console.log(mapped.repas_lie);
-        //console.log(mapped.evenement_lie);
-        //console.log(mapped.recette);
-        //console.log(mapped.ingredients);
-    
-        var html_recette="";
-    
-        html_recette += '<div id="evenement_repas" style="float: left; text-align:left">'+nom_evenement +" - " + repas.date + " - Repas : " + repas_lie.nom+'</div>';
-        html_recette += '<div id="session_cuisine" style="float: right; text-align:left">'+"Prépa : [" + nom_session_cuisine + "]"+'</div>';
-        html_recette += '<div style="clear:both;"></div>';
-        html_recette += '<h2 id="title" style="text-align:center; margin-bottom : 40px; margin-top:50px">'+recette.Nom+'</h2>';
-        html_recette += '<div style="width:100%">';
-        html_recette += '<div style="display:inline-block; width:49%; vertical-align:top;">';
-    
-        if(recette.Is_vegetarien){
-            html_recette += '<h4 id="is_vege">Vegetarien : <span style="color:green"> ✓ Oui</span></h4>';
-        }else{
-            html_recette += '<h4 id="is_vege">Vegetarien : <span style="color:red"> ✕ Non</span></h4>';
-        }
-    
-        if(recette.Is_vegan){
-            html_recette += '<h4 id="is_vegan">'+'Vegan : <span style="color:green"> ✓ Oui</span>'+'</h4>';
-        }else{
-            html_recette += '<h4 id="is_vegan">'+'Vegan : <span style="color:red"> ✕ Non</span>'+'</h4>';
-        }
-    
-        html_recette += '<h4 id="nb_portions"> Pour : '+nb_portions+' personnes</h4>';
-    
-        html_recette += '<h4 id="estim_duree"> Estimation : '+recette.Duree_de_preparation_estimee+' de prepa</h4>';
-    
-        html_recette += '<h4 id="multip_recette"> Représente '+multip_recette+' x la recette originale</h4>';
-    
-        html_recette += 'Notes : '+note_contexte_recette;
-    
-        html_recette += '</div>';
-    
-    
-        html_recette += '<table style="display:inline-block; width:49%; vertical-align:top;">';
-        html_recette += '<thead>';
-        html_recette += '<tr>';
-        html_recette += '<th colspan="2">Ingredients</th>';
-        html_recette += '</tr>';
-        html_recette += '</thead>';
-        html_recette += '<tbody id="ingredients_table_body">';
-    
-        var html_ingredients_list = "";
-        for(ingredient_in_recipe of recipe_ingredients){
-            html_ingredients_list += "<tr>";
-            html_ingredients_list += "<td>" + ingredient_in_recipe.Ingredient.Nom + "</td>";
-            var ingredient_scaled = (ingredient_in_recipe.qte_par_personne * nb_portions).toFixed(2);
-            html_ingredients_list += "<td>" + ingredient_scaled + ingredient_in_recipe.Ingredient.Unite.abbreviation + "</td>";
-            if(ingredient_in_recipe.note){
-                html_ingredients_list += "<td>(" + ingredient_in_recipe.note + ")</td>";
-            }
-        }
-    
-        html_recette += html_ingredients_list;
-        html_recette += '</tbody>';
-        html_recette += '</table>';
-    
-        html_recette += '<p id="texte_recette" style="white-space: pre-line; font-size : 1.5em; line-height: 150%;">'+recette.Texte+'</p>';
-    
-        html_recette += '<div style="break-after:page"></div>';
-        return(html_recette);
-    }
-    
     function generate_allergens_sheet(mapped){
         console.log("Generating allergens sheet");
      
@@ -98,38 +23,19 @@ $( document ).ready(function() {
         //document.getElementById('estim_nb_public').innerHTML = mapped.taille_public;
     
                     var details_recettes = JSON.parse(mapped.details_allergenes_par_recette);
-    /*
-                    var html_repas = "";
-                    for(recette of details_recette){
+                    var nom_evenement = nom_event;
 
-                        var json_details_recettes = JSON.parse(repas.details_recettes);
-  
-                        for(recette_contexte of json_details_recettes){
+    
+                    var html_fiche_allergenes = "";
+                    for(recette of details_recettes){
 
-                            console.log("id / sessions to include / test");
-                            console.log(recette_contexte.id_session_cuisine);
-                            console.log(sessions_to_include);
-                            console.log(sessions_to_include.includes(recette_contexte.id_session_cuisine.toString()));
-                            if((recette_contexte.prepare_avant && sessions_to_include.includes(recette_contexte.id_session_cuisine.toString()))
-                                || (!recette_contexte.prepare_avant && include_on_site_recipes)
-                            )
-                            {
-                                console.log("including "+recette_contexte.details_recette.Nom);
-                                sommaire_HTML += compteur_sommaire + " - " + repas.nom +" - " + recette_contexte.details_recette.Nom + "<br />";
-                                compteur_sommaire++;
-                                html_repas += recipeToHTML(mapped.nom_event, recette_contexte, repas);
-                            }else {
-                                sommaire_HTML += "<em>&emsp;" + repas.nom +" - " + recette_contexte.details_recette.Nom + " [Non inclus] </em><br />";
-                                console.log("NOT including "+recette_contexte.details_recette.Nom);
-                            }
-                            
-                        }
+                        var json_details_recettes = JSON.parse(recette);
+                        console.log(recette);
+                        
 
                     }
-                    sommaire_HTML += "</div>"
-    
-                    document.getElementById('sommaire').innerHTML = sommaire_HTML;
-                    document.getElementById('all_recettes').innerHTML = html_repas;*/
+
+                    document.getElementById('all_recettes').innerHTML = html_repas;
     }
     
     ready(function(){
@@ -138,16 +44,6 @@ $( document ).ready(function() {
                 {
                     name:'nom_event',
                     title:'Nom de l\'événement',
-                    optional: false,
-                },
-                {
-                    name:'date_debut',
-                    title:'Date de début',
-                    optional: false,
-                },
-                {
-                    name:'date_fin',
-                    title:'Date de fin',
                     optional: false,
                 },
                 {
