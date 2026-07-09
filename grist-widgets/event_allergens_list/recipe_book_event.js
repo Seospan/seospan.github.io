279729a8de7223e61c12a9fd91147da76bc9ca5c
Seospan <sophie.jobez@gmail.com>
2026-07-09 17:17:08 +0200

    Pop

--- grist-widgets/event_allergens_list/recipe_book_event.js
+++ grist-widgets/event_allergens_list/recipe_book_event.js
@@ -15,66 +15,6 @@ $( document ).ready(function() {
         console.log("ERROR");
     }
     
-    /*function updateRecipe(row, mappings){
-        console.log("TESTlyy");
-        console.log("row " + row);
-        console.log("mappings" + mappings);
-        console.log("arg : " + arguments);
-    
-    
-        try {
-            if (row === null) {
-                throw new Error("(No data - not on row - please add or select a row)");
-            }
-            console.log("GOT...", JSON.stringify(row));
-            if (row.References) {
-                try {
-                Object.assign(row, row.References);
-                } catch (err) {
-                throw new Error('Could not understand References column. ' + err);
-                }
-            }
-        
-            const mapped = grist.mapColumnNames(row);
-            // First check if all columns were mapped.
-            if (mapped) {
-                console.log(mappings);
-                document.getElementById('title').src = mapped.Titre;
-                document.getElementById('text').innerText = mapped.Texte;
-                if(mapped.is_vege){
-                    document.getElementById('is_vege').innerText = 'Vegetarien : <span style="color:green"> ✓ Oui</span>';
-                }else{
-                    document.getElementById('is_vege').innerText = 'Vegetarien : <span style="color:red"> ✕ Non</span>';
-                }
-    
-                if(mapped.is_vegan){
-                    document.getElementById('is_vegan').innerText = 'Vegan : <span style="color:green"> ✓ Oui</span>';
-                }else{
-                    document.getElementById('is_vegan').innerText = 'Vegan : <span style="color:red"> ✕ Non</span>';
-                }
-    
-                if(mapped.is_lactose_free){
-                    document.getElementById('is_lactose_free').innerText = ' Sans lactose <span style="color:green"> ✓ Oui</span>';
-                }else{
-                    document.getElementById('is_lactose_free').innerText = 'Sans lactose <span style="color:red"> ✕ Non</span>';
-                }
-    
-                document.getElementById('nb_convives').innerText = ' Pour : <?= data[i].scale ?> personnes'
-    
-    
-                console.log(`Using ${mappings.Titre} and ${mappings.Texte} columns`);
-            } else {
-                // Helper returned a null value. It means that not all
-                // required columns were mapped.
-                console.error("Please map all columns");
-            }
-    
-        } catch (err) {
-            handleError(err);
-        }
-    
-    }*/
-    
     function recipeToHTML(nom_evenement, recette_contexte, repas_lie){
     
         note_contexte_recette = recette_contexte.note_contexte_recette;
@@ -150,42 +90,22 @@ $( document ).ready(function() {
         return(html_recette);
     }
     
-    function generate_recipe_book(mapped){
-        console.log("Generating recipe book");
+    function generate_allergens_sheet(mapped){
+        console.log("Generating allergens sheet");
+     
+        //document.getElementById('nom_evenement').innerHTML = mapped.nom_event;
+        //document.getElementById('dates_evenement').innerHTML = "Du " + mapped.date_debut.toLocaleDateString("fr-FR",date_options) + " au " + mapped.date_fin.toLocaleDateString("fr-FR",date_options) ;
+        //document.getElementById('estim_nb_public').innerHTML = mapped.taille_public;
     
-        sessions_to_include = [];
-        $('#checkboxes_sessions :checkbox:checked').each(function(i){
-          sessions_to_include[i] = $(this).val();
-        });
-        include_on_site_recipes = $('#no_prepa_recipes').is(":checked");
-
-        console.log("ids of sessions to include :");
-        console.log(sessions_to_include);
-        console.log('take on site recipes :'+include_on_site_recipes);
-        
-        document.getElementById('nom_evenement').innerHTML = mapped.nom_event;
-                    document.getElementById('dates_evenement').innerHTML = "Du " + mapped.date_debut.toLocaleDateString("fr-FR",date_options) + " au " + mapped.date_fin.toLocaleDateString("fr-FR",date_options) ;
-                    document.getElementById('estim_nb_public').innerHTML = mapped.taille_public;
-    
-                    var details_repas = JSON.parse(mapped.details_repas);
-                    var sommaire_HTML = "<div id='sommaire-inside'>";
-                    var compteur_sommaire = 1;
-    
+                    var details_recettes = JSON.parse(mapped.details_allergenes_par_recette);
+    /*
                     var html_repas = "";
-                    for(repas of details_repas){
-                        //console.log("RECETTE FROM REPAS");
-                        //console.log(repas);
+                    for(recette of details_recette){
+
                         var json_details_recettes = JSON.parse(repas.details_recettes);
-                        //console.log("DETAILS RECETTES");
-                        //console.log(json_details_recettes);
-
+  
                         for(recette_contexte of json_details_recettes){
-                            //console.log("UNE RECETTE");
-                            //console.log(recette_contexte);
 
-                            /* Si la recette est notée comme préparée à l'avance, vérifier que la session de prepa est comprise dans les recettes demandées
-                            Si la recette n'est pas notée comme préparée à l'avance, vérifier que les recettes préparées sur site sont demandées
-                             */
                             console.log("id / sessions to include / test");
                             console.log(recette_contexte.id_session_cuisine);
                             console.log(sessions_to_include);
@@ -204,50 +124,20 @@ $( document ).ready(function() {
                             }
                             
                         }
-                        //recipeToHTML(mapped.nom_event, repas.nom_session_cuisine, recette, nb_portions, multip_recette, recipe_ingredients, repas_lie)
-                        //recipeToHTML(nom_evenement, nom_session_cuisine, recette, nb_portions, multip_recette, recipe_ingredients, repas_lie){
-                        
-                        //html_repas += repas.nom;
+
                     }
                     sommaire_HTML += "</div>"
     
                     document.getElementById('sommaire').innerHTML = sommaire_HTML;
-                    document.getElementById('all_recettes').innerHTML = html_repas;
+                    document.getElementById('all_recettes').innerHTML = html_repas;*/
     }
-    
-    const date_options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     
     ready(function(){
         //console.log("GOGOGO");
         grist.ready({columns: [
                 {
-                    name:'details_repas',
-                    title:'Détails des repas liés',
-                    optional: false,
-                },
-                {
-                    name:'date_debut',
-                    title:'Date de début',
-                    optional: false,
-                },
-                {
-                    name:'date_fin',
-                    title:'Date de fin',
-                    optional: false,
-                },
-                {
-                    name:'nom_event',
-                    title:'Nom de l\'événement',
-                    optional: false,
-                },
-                {
-                    name:'taille_public',
-                    title:'Taille du public',
-                    optional: false,
-                },
-                {
-                    name:'sessions_prepa_liees',
-                    title:'Sessions de cuisine liées',
+                    name:'details_allergenes_par_recette',
+                    title:'Détails des allergenes par recette',
                     optional: false,
                 },
             ], requiredAccess: 'read table'});
@@ -279,19 +169,8 @@ $( document ).ready(function() {
                     //console.log("DATE");
                     //console.log(mapped.date_debut);
     
-                    document.getElementById("generate_recipe_book").addEventListener("click", () => generate_recipe_book(mapped));
+                    document.getElementById("generate_allergens_sheet").addEventListener("click", () => generate_recipe_book(mapped));
                
-                    //Add checkboxes to choose which cooking session(s) to include, and if "no-cooking-session" (== cookes on event) are included
-                    sessions_prepa = mapped.sessions_prepa_liees;
-                    checkboxes_sessions_html = "";
-                    for(session of sessions_prepa){
-                        checkboxes_sessions_html += '<div><input type="checkbox" class="sessions_choice" id="'+session.id+'" name="sessions_choice" value="'+session.id+'" /> <label for="'+session.id+'">'+session.nom+'</label>';
-                    }
-                    document.getElementById('checkboxes_sessions').innerHTML = checkboxes_sessions_html;
-                    
-                    //Add a checkbox for taking recipes with no prepa session (==recipes done on site)
-                    document.getElementById('checkbox_on_site').innerHTML = '<div><input type="checkbox" class="no_prepa_recipes" id="no_prepa_recipes" name="no_prepa_recipes" value="no_prepa_recipes" /> <label for="no_prepa_recipes"> Recettes préparées sur site </label>';
-        
                     console.log(`Using ${mappings.Titre} and ${mappings.Texte} columns`);
                 } else {
                     // Helper returned a null value. It means that not all
